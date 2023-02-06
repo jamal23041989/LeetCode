81795ad2041eac7f7bf29a83524278a8e414cc2d
Jamal Kurbanov <jamalkurbanov23041989@gmail.com>
2023-02-06 04:29:59 +0300

    1. Two Sum

--- Python/Easy/1. Two Sum/index.py
+++ Python/Easy/1. Two Sum/index.py
@@ -13,7 +13,6 @@ class Solution:
 
 res = Solution()
 print(res.twoSum([2, 7, 11, 15], 9)) # [0,1]
-print(res.twoSum([2, 11, 7, 15], 9)) # [0,2]
 print(res.twoSum([3, 2, 4], 6)) # [1,2]
 print(res.twoSum([3, 3], 6)) # [0,1]
 
