9f203ae86632961a052398a16df8851125ae4b71
Jamal Kurbanov <jamalkurbanov23041989@gmail.com>
2023-02-15 12:41:16 +0300

    205. Isomorphic Strings

--- Python/Easy/205. Isomorphic Strings/index.py
+++ Python/Easy/205. Isomorphic Strings/index.py
@@ -1,15 +1,24 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        sl = dict()
-        print(sl)
+        d = dict()
+        if len(s) == 0 and len(t) == 0:
+            return False
 
         for i in range(len(s)):
-            i
+            if s[i] in d:
+                if d[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in d.values():
+                    return False
+                d[s[i]] = t[i]
+        return True
+
 
 
 
 
 a = Solution()
 print(a.isIsomorphic(s="egg", t="add"))  # true
-# print(a.isIsomorphic(s="foo", t="bar"))  # false
-# print(a.isIsomorphic(s="paper", t="title"))  # true
+print(a.isIsomorphic(s="foo", t="bar"))  # false
+print(a.isIsomorphic(s="paper", t="title"))  # true
